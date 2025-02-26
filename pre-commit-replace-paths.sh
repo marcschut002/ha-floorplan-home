f848f4d9cf6447f84bbd5f3f2490989c4f0f44fa
Marc Schut <marc.schut@topicus.nl>
2025-02-26 20:14:18 +0100

    fix paths

--- pre-commit-replace-paths.sh
+++ pre-commit-replace-paths.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 find='/local/'
-replace='/hacsfiles/ha-floorplan-home/'
+replace='/hacsfiles/ha-floorplan-home/beganegrond/'
 sed -i '' 's#'$find'#'$replace'#g' dist/beganegrond/floorplan.yaml
