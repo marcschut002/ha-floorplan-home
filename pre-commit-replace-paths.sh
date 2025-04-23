0e684f17c5a3177486e7be395ba0b5cefcd277fe
Marc Schut <marc.schut@topicus.nl>
2025-04-23 14:09:06 +0200

    bump

--- pre-commit-replace-paths.sh
+++ pre-commit-replace-paths.sh
@@ -2,3 +2,7 @@
 find='/local/'
 replace='/hacsfiles/ha-floorplan-home/beganegrond/'
 sed -i '' 's#'$find'#'$replace'#g' dist/beganegrond/floorplan.yaml
+
+find='/local/'
+replace='/hacsfiles/ha-floorplan-home/zonnepanelen/'
+sed -i '' 's#'$find'#'$replace'#g' dist/zonnepanelen/floorplan.yaml
